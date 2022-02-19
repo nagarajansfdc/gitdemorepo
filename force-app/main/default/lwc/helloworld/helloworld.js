ceddbd9000629207995d263da8ac83a690cca224
unknown <nagarajand1975@gmail.com>
2022-02-19 19:51:45 +0530

    Hellow Rold JS Change

--- force-app/main/default/lwc/helloworld/helloworld.js
+++ force-app/main/default/lwc/helloworld/helloworld.js
@@ -3,7 +3,7 @@ import { LightningElement } from 'lwc';
 export default class Helloworld extends LightningElement {
 
     handleClick(){
-        //alert('Hai Good Morning')
+        alert('Hai Good Morning')
         console.log('Hai Good Morning') 
     }
 
